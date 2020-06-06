a773b0e2c0efc0df8383cb464ef51ca8a538bd43
kyledkearney <kyledkearney@gmail.com>
2020-06-05 21:11:49 -0600

    setup semantic table

--- src/views/artist-page/components/ItemList.js
+++ src/views/artist-page/components/ItemList.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import ListItem from './ListItem'
+import { Table } from 'semantic-ui-react'
 
  function ItemList(props) {
 
 
+    const albumHeader = (
+     <>   
+        <Table.HeaderCell>Album Title</Table.HeaderCell>
+        <Table.HeaderCell>Play Count</Table.HeaderCell>
+        <Table.HeaderCell>Last FM Link</Table.HeaderCell>
+     </>   
+    )
+    const songHeader = (
+     <>   
+        <Table.HeaderCell>Song Title</Table.HeaderCell>
+        <Table.HeaderCell>Play Count</Table.HeaderCell>
+        <Table.HeaderCell>Listeners</Table.HeaderCell>
+        <Table.HeaderCell>Last FM Link</Table.HeaderCell>
+     </>   
+    )
+
 
   return (
-      <ul className='list'>
+      <div className='list'>
+            <Table.Header>
+            <Table.Row>
+            {props.type === 'TopTracks' ? songHeader : albumHeader}
+
+            </Table.Row>
+            </Table.Header>
           <ListItem queryType={props.type} artist={props.artist}/>
-      </ul>
+      </div>
 
   )
 }
